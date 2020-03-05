703019abae824bd0aa2525d998d29fab8dbbd27d
Alexandru Neamtu <alexneamtu@gmail.com>
2020-03-05 23:25:45 +0200

    Updates

---
+++ precache-manifest.0bed7e67ed889930d13d42e383d9a158.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961b43af2d4bf618e9e1a649dae78488",
+    "url": "/risc-seismic-bucuresti-web/index.html"
+  },
+  {
+    "revision": "33cb71e6f8ca7c217912",
+    "url": "/risc-seismic-bucuresti-web/static/css/main.ae500f7f.chunk.css"
+  },
+  {
+    "revision": "ab4f4341b0f41af1633d",
+    "url": "/risc-seismic-bucuresti-web/static/js/2.a78358d6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/risc-seismic-bucuresti-web/static/js/2.a78358d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33cb71e6f8ca7c217912",
+    "url": "/risc-seismic-bucuresti-web/static/js/main.b7511267.chunk.js"
+  },
+  {
+    "revision": "af1e4f96ae18169602cc",
+    "url": "/risc-seismic-bucuresti-web/static/js/runtime-main.8f58ae60.js"
+  },
+  {
+    "revision": "4612253a3a38476860359fdf6315e13d",
+    "url": "/risc-seismic-bucuresti-web/static/media/logo.4612253a.svg"
+  },
+  {
+    "revision": "c93e35923e215aee51f876e7f546600f",
+    "url": "/risc-seismic-bucuresti-web/static/media/search.c93e3592.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/risc-seismic-bucuresti-web/precache-manifest.252c51b4926357b86bcded4ffae94b40.js"
+  "/risc-seismic-bucuresti-web/precache-manifest.0bed7e67ed889930d13d42e383d9a158.js"
 );
 
 self.addEventListener('message', (event) => {
