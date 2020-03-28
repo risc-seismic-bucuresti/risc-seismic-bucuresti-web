4a859eb58b71f8b8ff5c8f016c44540b0856736c
Alex Neamtu <alexneamtu@gmail.com>
2020-03-28 20:46:24 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29ef0da5661be96ca194cf1960f949f5.js"
+  "/precache-manifest.a0413a6e54989ec50169c073725c50ad.js"
 );
 
 self.addEventListener('message', (event) => {
