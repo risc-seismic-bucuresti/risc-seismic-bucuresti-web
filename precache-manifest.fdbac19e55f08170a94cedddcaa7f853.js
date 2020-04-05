c0706926ae5a4380ceceb2f69104a92b17054a7f
Alex Neamtu <alexneamtu@gmail.com>
2020-04-05 16:16:20 +0300

    Updates

---
+++ precache-manifest.fdbac19e55f08170a94cedddcaa7f853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9810bb5b22a10795b21d00584764a191",
+    "url": "https://riscseismicbucuresti.ro/index.html"
+  },
+  {
+    "revision": "9c51333e15c4001fa092",
+    "url": "https://riscseismicbucuresti.ro/static/css/main.5ab60425.chunk.css"
+  },
+  {
+    "revision": "66296696c1634add28d7",
+    "url": "https://riscseismicbucuresti.ro/static/js/2.dcbeb586.chunk.js"
+  },
+  {
+    "revision": "effad757c3115357d544dd61ee14fd10",
+    "url": "https://riscseismicbucuresti.ro/static/js/2.dcbeb586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c51333e15c4001fa092",
+    "url": "https://riscseismicbucuresti.ro/static/js/main.a4595862.chunk.js"
+  },
+  {
+    "revision": "8f8ac670b2dc2dd5df8f",
+    "url": "https://riscseismicbucuresti.ro/static/js/runtime-main.86570174.js"
+  },
+  {
+    "revision": "a51689e9a345dd904fd2e86a7d78be23",
+    "url": "https://riscseismicbucuresti.ro/static/media/rsb_logo_centered.a51689e9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c96a2aad3351024b5d700b47027b8afd.js"
+  "https://riscseismicbucuresti.ro/precache-manifest.fdbac19e55f08170a94cedddcaa7f853.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://riscseismicbucuresti.ro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
